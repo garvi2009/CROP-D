0b25da920f48e64447518fe920e175f4a54f5c46
Shoray Singhal <shoryasinghall@gmail.com>
2022-09-29 23:17:52 +0530

    navbar config

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,37 @@
 <script>
-	import '../app.postcss';
+  import "../app.postcss";
+  import {
+    Navbar,
+    NavBrand,
+    NavLi,
+    NavUl,
+    NavHamburger,
+  } from "flowbite-svelte";
 </script>
 
+<Navbar let:hidden let:toggle>
+  <NavBrand href="/">
+    <img src="src/assets/crop.png" class="h-14 " alt="CROP-D Logo" />
+    <span
+      class="self-center whitespace-nowrap text-xl font-semibold nav-color"
+    >
+      CROP-D
+    </span>
+  </NavBrand>
+  <NavHamburger on:click={toggle} />
+  <NavUl {hidden}>
+    <NavLi href="/" active={true}>Home</NavLi>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/">Services</NavLi>
+    <NavLi href="/">Pricing</NavLi>
+    <NavLi href="/">Contact</NavLi>
+  </NavUl>
+</Navbar>
+
 <slot />
+
+<style>
+  .nav-color {
+    background-color: var(--nav-color-1);
+  }
+</style>
---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,25 @@
+<script>
+  import SvelteMarkdown from "svelte-markdown";
+  let source = "Loading...";
+  fetch(
+    "https://raw.githubusercontent.com/Shoray2002/molars/molar-dev/README.md"
+  )
+    .then((response) => response.text())
+    .then((data) => {
+      source = data;
+    });
+</script>
+
+<div class="container">
+  <SvelteMarkdown {source} />
+</div>
+
+<style>
+  .container {
+    max-width: 800px;
+    margin: 0 auto;
+    padding: 0 1rem;
+    background-color: antiquewhite;
+  }
+
+</style>
