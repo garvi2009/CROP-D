2355959ad98aa002e9d74d1927fb9914eaa254d4
garvi2009 <114023911+garvi2009@users.noreply.github.com>
2022-10-04 02:11:47 +0530

    made the button working

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -11,64 +11,22 @@
       <div class="card card-100">
         <div class="card-content">
           <div class="card-heading">
-            <h3>Card Heading</h3>
+            <h3>Test a leaf</h3>
           </div>
           <div class="card-body">
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi,
-              laborum.
-            </p>
+            <p>Upload an image of a leaf to test for disease.</p>
           </div>
         </div>
-        <div class="card-action">
-          <button>Read More &rarr;</button>
+        <div
+          class="card-action"
+          on:click={() => {
+            window.location.href = "/dashboard/test";
+          }}
+        >
+          <button>Get Started</button>
         </div>
         <div class="card-bgimg" style="background:whitesmoke;" />
       </div>
-
-      <!-- <div class="content-shell">
-        <div class="card card-50">
-          <div class="card-content">
-            <div class="card-heading">
-              <h3>Card Heading</h3>
-            </div>
-            <div class="card-body">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi,
-                laborum.
-              </p>
-            </div>
-          </div>
-          <div class="card-action">
-            <button>Read More &rarr;</button>
-          </div>
-          <div
-            class="card-bgimg"
-            style="background-image: url('https://images.pexels.com/photos/929824/pexels-photo-929824.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');"
-          />
-        </div>
-
-        <div class="card card-50">
-          <div class="card-content">
-            <div class="card-heading">
-              <h3>Card Heading</h3>
-            </div>
-            <div class="card-body">
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi,
-                laborum.
-              </p>
-            </div>
-          </div>
-          <div class="card-action">
-            <button>Read More &rarr;</button>
-          </div>
-          <div
-            class="card-bgimg"
-            style="background-image: url('https://images.pexels.com/photos/735911/pexels-photo-735911.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260');"
-          />
-        </div>
-      </div> -->
     </div>
   </div>
 </div>
@@ -96,10 +54,6 @@
 
   .content-shell .card {
     align-self: stretch;
-  }
-
-  .content-shell .content-shell {
-    flex-direction: column;
   }
 
   .content-shell .content-shell .card {
@@ -176,13 +130,13 @@
     user-select: none;
   }
 
-  .card:before {
+  /* .card:before {
     opacity: 0;
     border: 1px solid #ededed;
     transform-origin: center center;
     transition: transform 0.3s cubic-bezier(0.075, 0.82, 0.165, 1),
       opacity 0.4s cubic-bezier(0.075, 0.82, 0.165, 1);
-  }
+  } */
 
   .card:after {
     background: #121212;
@@ -210,10 +164,12 @@
     left: 50%;
     transform: translate(-50%, 100%);
     opacity: 0;
-    pointer-events: none;
+    pointer-events: all;
     transition: opacity 0.75s cubic-bezier(0.075, 0.82, 0.165, 1),
       transform 0.6s cubic-bezier(0.075, 0.82, 0.165, 1);
   }
+
+  /* .card-action:hover{ */
 
   .card h3 {
     margin-top: 0;
